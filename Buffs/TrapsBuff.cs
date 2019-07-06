d544e81fd8466bbe58e971fb07f52f22267d2626
Cerberus <51979790+tyhnoone@users.noreply.github.com>
2019-07-06 17:31:38 +0800

    Add Chinese Localization

--- Buffs/TrapsBuff.cs
+++ Buffs/TrapsBuff.cs
@@ -17,7 +17,9 @@ namespace AlchemistNPCLite.Buffs
 			Main.debuff[Type] = false;
 			canBeCleared = true;
 			DisplayName.AddTranslation(GameCulture.Russian, "Усилитель ловушек");
-            Description.AddTranslation(GameCulture.Russian, "Ловушки значительно усилены");
+           		Description.AddTranslation(GameCulture.Russian, "Ловушки значительно усилены");
+			DisplayName.AddTranslation(GameCulture.Chinese, "陷阱增强");
+            		Description.AddTranslation(GameCulture.Chinese, "增强陷阱");
 		}
 		
 		public override void Update(Player player, ref int buffIndex)
