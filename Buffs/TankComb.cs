a12baca248f4766be8bb8955c4fffb5727b08f1e
VVV101 <VVVHUNTER@yandex.ru>
2019-07-08 18:50:39 +0600

    combos fix

--- Buffs/TankComb.cs
+++ Buffs/TankComb.cs
@@ -21,73 +21,85 @@ namespace AlchemistNPCLite.Buffs
 			Description.SetDefault("Combination of Endurance, Lifeforce, Ironskin, Obsidian Skin, Thorns, Regeneration, Titan Skin, Invincibility buffs");
 			Main.debuff[Type] = false;
 			canBeCleared = true;
-			DisplayName.AddTranslation(GameCulture.Russian, "Комбинация Танка (с Модовыми)");
+			DisplayName.AddTranslation(GameCulture.Russian, "Комбинация Танка (с Модами)");
             Description.AddTranslation(GameCulture.Russian, "Сочетание баффов Выносливости, Жизненных Сил, Железной Кожи, Обсидиановой Кожи, Шипов, Регенерации, Кожи Титана и Неуязвимости");
             DisplayName.AddTranslation(GameCulture.Chinese, "坦克药剂包 (模组)");
-            Description.AddTranslation(GameCulture.Chinese, "包含以下Buff：耐力, 生命力, 铁皮, 黑曜石皮肤, 荆棘, 恢复, 抵抗, 泰坦皮肤, 无敌");
+            Description.AddTranslation(GameCulture.Chinese, "包含以下Buff：耐力, 生命力, 铁皮, 黑曜石皮肤, 荆棘, 恢复, 泰坦皮肤, 无敌");
         }
 		
 		public override void Update(Player player, ref int buffIndex)
 		{
-			if (player.FindBuffIndex(mod.BuffType("BattleComb")) >= 0)
+			if (!player.HasBuff(mod.BuffType("BattleComb")))
 			{
-			player.longInvince = true;
-			player.lavaImmune = true;
-            player.fireWalk = true;
-			if (NPC.downedMechBoss2)
-			{
-			player.buffImmune[39] = true;
-			player.buffImmune[69] = true;
-			}
-            player.buffImmune[24] = true;
-			player.buffImmune[44] = true;
-			player.buffImmune[46] = true;
-			player.buffImmune[47] = true;
-			player.buffImmune[1] = true;
-			player.buffImmune[2] = true;
-			player.buffImmune[5] = true;
-			player.buffImmune[14] = true;
-			player.buffImmune[113] = true;
-			player.buffImmune[114] = true;
-			player.buffImmune[mod.BuffType("LongInvincible")] = true;
-			player.buffImmune[mod.BuffType("TitanSkin")] = true;
-			player.endurance += 0.1f;
+				player.longInvince = true;
+				if (NPC.downedMechBoss2)
 				{
-                        if ((double)player.thorns < 1.0)
-                            player.thorns = 0.3333333f;
-                }
-			}
-			else
-			{
-			player.longInvince = true;
-			player.lavaImmune = true;
-            player.fireWalk = true;
-            if (NPC.downedMechBoss2)
-			{
-			player.buffImmune[39] = true;
-			player.buffImmune[69] = true;
-			}
-			player.buffImmune[24] = true;
-			player.buffImmune[44] = true;
-			player.buffImmune[46] = true;
-			player.buffImmune[47] = true;
-			player.buffImmune[1] = true;
-			player.buffImmune[2] = true;
-			player.buffImmune[5] = true;
-			player.buffImmune[14] = true;
-			player.buffImmune[113] = true;
-			player.buffImmune[114] = true;
-			player.buffImmune[mod.BuffType("LongInvincible")] = true;
-			player.buffImmune[mod.BuffType("TitanSkin")] = true;
-			player.endurance += 0.1f;
-			player.statDefense += 8;
-			player.lifeRegen += 4;
-			player.lifeForce = true;
-            player.statLifeMax2 += player.statLifeMax / 5 / 20 * 20;
+					player.buffImmune[39] = true;
+					player.buffImmune[69] = true;
+				}
+				player.buffImmune[24] = true;
+				player.buffImmune[44] = true;
+				player.buffImmune[46] = true;
+				player.buffImmune[47] = true;
+				player.lavaImmune = true;
+				player.fireWalk = true;
+				player.buffImmune[1] = true;
+				player.buffImmune[2] = true;
+				player.buffImmune[5] = true;
+				player.buffImmune[14] = true;
+				player.buffImmune[113] = true;
+				player.buffImmune[114] = true;
+				player.buffImmune[mod.BuffType("LongInvincible")] = true;
+				player.buffImmune[mod.BuffType("TitanSkin")] = true;
+				player.endurance += 0.1f;
+				player.statDefense += 8;
+				if (ModLoader.GetMod("CalamityMod") != null)
 				{
-                        if ((double)player.thorns < 1.0)
-                            player.thorns = 0.3333333f;
-                }
+					if (!player.HasBuff(mod.BuffType("CalamityComb")) && !player.HasBuff(ModLoader.GetMod("CalamityMod").BuffType("Cadence")))
+					{
+						player.lifeRegen += 4;
+						player.lifeForce = true;
+						player.statLifeMax2 += player.statLifeMax / 5 / 20 * 20;
+					}
+				}
+				if (ModLoader.GetMod("CalamityMod") == null)
+				{
+					player.lifeRegen += 4;
+					player.lifeForce = true;
+					player.statLifeMax2 += player.statLifeMax / 5 / 20 * 20;
+				}
+				if (player.thorns < 1.0)
+				{
+				player.thorns = 0.3333333f;
+				}
+			}
+			if (player.HasBuff(mod.BuffType("BattleComb")))
+			{
+				player.longInvince = true;
+				if (NPC.downedMechBoss2)
+				{
+					player.buffImmune[39] = true;
+					player.buffImmune[69] = true;
+				}
+				player.buffImmune[24] = true;
+				player.buffImmune[44] = true;
+				player.buffImmune[46] = true;
+				player.buffImmune[47] = true;
+				player.lavaImmune = true;
+				player.fireWalk = true;
+				player.buffImmune[1] = true;
+				player.buffImmune[2] = true;
+				player.buffImmune[5] = true;
+				player.buffImmune[14] = true;
+				player.buffImmune[113] = true;
+				player.buffImmune[114] = true;
+				player.buffImmune[mod.BuffType("LongInvincible")] = true;
+				player.buffImmune[mod.BuffType("TitanSkin")] = true;
+				player.endurance += 0.1f;
+				if (player.thorns < 1.0)
+				{
+				player.thorns = 0.3333333f;
+				}
 			}
 		}
 	}
--- Buffs/VanTankComb.cs
+++ Buffs/VanTankComb.cs
@@ -24,58 +24,61 @@ namespace AlchemistNPCLite.Buffs
 			DisplayName.AddTranslation(GameCulture.Russian, "Комбинация Танка (без Модовых)");
             Description.AddTranslation(GameCulture.Russian, "Сочетание баффов Выносливости, Жизненных Сил, Железной Кожи, Обсидиановой Кожи, Шипов и Регенерации");
             DisplayName.AddTranslation(GameCulture.Chinese, "坦克药剂包 (原版)");
-            Description.AddTranslation(GameCulture.Chinese, "包含以下Buff：耐力, 生命力, 铁皮, 黑曜石皮肤, 荆棘, 恢复, 抵抗");
+            Description.AddTranslation(GameCulture.Chinese, "包含以下Buff：耐力, 生命力, 铁皮, 黑曜石皮肤, 荆棘, 恢复");
         }
 		
 		public override void Update(Player player, ref int buffIndex)
 		{
-			if (player.FindBuffIndex(mod.BuffType("BattleComb")) >= 0)
+			if (!player.HasBuff(mod.BuffType("BattleComb")) && !player.HasBuff(mod.BuffType("TankComb")))
 			{
-				if (player.FindBuffIndex(mod.BuffType("TankComb")) >= 0)
+				player.lavaImmune = true;
+				player.fireWalk = true;
+				player.buffImmune[1] = true;
+				player.buffImmune[2] = true;
+				player.buffImmune[5] = true;
+				player.buffImmune[14] = true;
+				player.buffImmune[113] = true;
+				player.buffImmune[114] = true;
+				player.endurance += 0.1f;
+				player.statDefense += 8;
+				if (ModLoader.GetMod("CalamityMod") != null)
 				{
+					if (!player.HasBuff(mod.BuffType("CalamityComb")) && !player.HasBuff(ModLoader.GetMod("CalamityMod").BuffType("Cadence")))
+					{
+						player.lifeRegen += 4;
+						player.lifeForce = true;
+						player.statLifeMax2 += player.statLifeMax / 5 / 20 * 20;
+					}
 				}
-				else
+				if (ModLoader.GetMod("CalamityMod") == null)
 				{
-			player.lavaImmune = true;
-            player.fireWalk = true;
-			player.buffImmune[1] = true;
-			player.buffImmune[2] = true;
-			player.buffImmune[5] = true;
-			player.buffImmune[14] = true;
-			player.buffImmune[113] = true;
-			player.buffImmune[114] = true;
-			player.endurance += 0.1f;
+					player.lifeRegen += 4;
+					player.lifeForce = true;
+					player.statLifeMax2 += player.statLifeMax / 5 / 20 * 20;
+				}
+				if (player.thorns < 1.0)
 				{
-                        if ((double)player.thorns < 1.0)
-                            player.thorns = 0.3333333f;
-                }
+				player.thorns = 0.3333333f;
 				}
 			}
-			else
+			if (player.HasBuff(mod.BuffType("BattleComb")))
 			{
-				if (player.FindBuffIndex(mod.BuffType("TankComb")) >= 0)
+				player.lavaImmune = true;
+				player.fireWalk = true;
+				player.buffImmune[1] = true;
+				player.buffImmune[2] = true;
+				player.buffImmune[5] = true;
+				player.buffImmune[14] = true;
+				player.buffImmune[113] = true;
+				player.buffImmune[114] = true;
+				player.endurance += 0.1f;
+				if (player.thorns < 1.0)
 				{
+				player.thorns = 0.3333333f;
 				}
-				else
-				{
-			player.lavaImmune = true;
-            player.fireWalk = true;
-			player.buffImmune[1] = true;
-			player.buffImmune[2] = true;
-			player.buffImmune[5] = true;
-			player.buffImmune[14] = true;
-			player.buffImmune[113] = true;
-			player.buffImmune[114] = true;
-			player.endurance += 0.1f;
-			player.statDefense += 8;
-			player.lifeRegen += 4;
-			player.lifeForce = true;
-            player.statLifeMax2 += player.statLifeMax / 5 / 20 * 20;
-				{
-                        if ((double)player.thorns < 1.0)
-                            player.thorns = 0.3333333f;
-                }
-				}
+			}
+			if (player.HasBuff(mod.BuffType("TankComb")))
+			{
 			}
 		}
 	}
