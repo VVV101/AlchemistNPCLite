9b5987ccc397b073659f979e5abea5f5d3032d63
VVV101 <VVVHUNTER@yandex.ru>
2019-10-27 18:44:08 +0600

    hotfix

--- Items/HeartAttackPotion.cs
+++ Items/HeartAttackPotion.cs
@@ -7,7 +7,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.Localization;
  
-namespace AlchemistNPC.Items
+namespace AlchemistNPCLite.Items
 {
     public class HeartAttackPotion : ModItem
     {
