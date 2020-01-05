2aadea1767ab84b0f1e55f361839be8900f74841
VVV101 <VVVHUNTER@yandex.ru>
2020-01-05 11:13:57 +0600

    fix+

--- Items/NinjaPotion.cs
+++ Items/NinjaPotion.cs
@@ -15,10 +15,10 @@ namespace AlchemistNPCLite.Items
         public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Ninja Potion");
-			Tooltip.SetDefault("Increases damage and critical chance by 5% and grants abilities of a Master Ninja"
+			Tooltip.SetDefault("Increases damage and critical chance by 5%\nGives a chance to dodge attacks and allows the ability to climb walls"
 			+"\nNON-CALAMITY BUFF POTION");
 			DisplayName.AddTranslation(GameCulture.Russian, "Зелье Ниндзя");
-            Tooltip.AddTranslation(GameCulture.Russian, "Увеличивает урон и шанс критического удара любым типом на 5% и даёт способности Мастера Ниндзя");
+            Tooltip.AddTranslation(GameCulture.Russian, "Увеличивает урон и шанс критического удара любым типом на 5%\тДаёт шанс увернуться от атаки и позволяет цепляться за стены");
 
             DisplayName.AddTranslation(GameCulture.Chinese, "忍者药剂");
             Tooltip.AddTranslation(GameCulture.Chinese, "增加5%的伤害和暴击几率, 并获得忍者大师的能力");
